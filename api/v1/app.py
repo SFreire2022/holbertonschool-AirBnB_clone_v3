0d6b548308e641334093936662fb7d491d4160bb
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-11 23:55:36 +0000

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,4 +22,5 @@ if __name__ == "__main__":
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return jsonify({'error': 'Not found'}), 404
+    """404"""
+    return jsonify({"error": "Not found"}), 404

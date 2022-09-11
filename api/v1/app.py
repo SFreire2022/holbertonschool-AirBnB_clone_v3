f5e63510ff87ce9f5bf39b64430de67b6c3226dc
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-11 23:57:37 +0000

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,13 +14,13 @@ def close(self):
     """close method"""
     storage.close()
 
+
+def page_not_found(error):
+    """404"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """404"""
-    return jsonify({"error": "Not found"}), 404

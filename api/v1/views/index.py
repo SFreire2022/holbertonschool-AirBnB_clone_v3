4a96e54773815dc37c4a89e719db80b900efa3bf
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-11 23:26:09 +0000

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ import models
 from api.v1.views import app_views
 from flask import jsonify
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -11,7 +17,7 @@ def status_ok():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats')
 def ret_count():
     dic = {'amenities': Amenity, 'cities': City,
            'places': Place, 'reviews': Review,

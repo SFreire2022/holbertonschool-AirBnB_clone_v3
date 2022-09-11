cceebf974f919d4957a1cd74a070dfd60e49c895
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-11 21:37:10 +0000

    task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,13 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(self):
     """close method"""
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', '5000'), threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)

6f1d9b3d0016e16487d8b764620f1cadb404d6f6
MauriPercovich <mauriciopercovich22@gmail.com>
2022-09-11 21:31:50 +0000

    task 4

---
+++ api/v1/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""app"""
+from flask import Flask, Blueprint, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close(self):
+    """close method"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', '5000'), threaded=True)

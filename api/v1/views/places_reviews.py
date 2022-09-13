c2f1a786d30c78f5fea599ca241a91b54e0d4465
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-13 14:34:39 -0300

    Fix Review class import

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,8 +3,7 @@
 import models
 from api.v1.views import app_views
 from flask import jsonify, abort, request, Response, make_response
-from models.state import State
-from models.city import City
+from models.review import Review
 from models.place import Place
 from models.user import User
 from models import storage

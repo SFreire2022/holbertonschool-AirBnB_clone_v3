d6ffe8fd44dcee7a49ef41521f05d431f3483370
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-12 22:27:06 -0300

    Update web app flask for CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,13 @@ from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close(self):
@@ -24,4 +27,4 @@ def page_not_found(error):
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
